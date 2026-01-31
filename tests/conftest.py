ee047eff7ddcf0279e9da89297747753a314c536
johnmullender <johnmullender2x@gmail.com>
2026-01-30 23:29:00 -0600

    Add auth helper class for unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ os.environ['FLASK_ENV'] = 'TESTING'
 
 from app import app, db
 
+# Create and destroy temporary database for tests
 @pytest.fixture()
 def client():
     app.config['TESTING']= True
@@ -16,3 +17,18 @@ def client():
         db.session.remove()
         db.drop_all()
 
+
+# Create helper class to automatically logic test User
+class AuthActions:
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username, password):
+        self._client.post('/register', data={'username': username, 'password': password})
+        self._client.post('/login', data={'username': username, 'password': password})
+
+
+# Use helper class for login
+@pytest.fixture()
+def auth(client):
+    return AuthActions(client)
