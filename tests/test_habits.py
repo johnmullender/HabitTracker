7ac5bcc798aaf383f3eaaf9bf5101436cddd4777
johnmullender <johnmullender2x@gmail.com>
2026-01-31 03:13:32 -0600

    created unit test to ensure users are not able to delete other users habits

--- tests/test_habits.py
+++ tests/test_habits.py
@@ -29,5 +29,30 @@ def test_delete_habit(client, auth):
     # Confirm habit is removed from database
     assert Habit.query.count() == 0
 
+
+def test_delete_other_users_habit(client):
+    with app.app_context():
+        # Create 2 Users and add them to database
+        attacker = User(username= 'attacker', password= '123')
+        victim = User(username= 'victim', password= '123')
+        db.session.add(attacker)
+        db.session.add(victim)
+        db.session.commit()
+
+        # Create a habit owned by victim
+        test_habit = Habit(name='test', user_id= victim.id, date_created= date.today())
+        db.session.add(test_habit)
+        db.session.commit()
+
+        victim_habit_id = test_habit.id
+        attacker_username = attacker.username
+
+    # Attacker attempts to delete victims habit
+    client.post('/login', data={'username': attacker_username, 'password': '123'})
+    client.post(f'/delete/{victim_habit_id}')
+
+    with app.app_context():
+        # Make sure victims habit still exists
+        assert Habit.query.filter_by(id= victim_habit_id).first() is not None
     
 
