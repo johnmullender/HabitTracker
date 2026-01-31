67934cc25fda6a8ff6a2898f963dcab6edfef9c5
johnmullender <johnmullender2x@gmail.com>
2026-01-30 23:54:26 -0600

    Added unit tests for creation and deletion of habits

---
+++ tests/test_habits.py
@@ -0,0 +1,32 @@
+from app import Habit
+
+def test_add_habit(client, auth):
+    # Log in dummy tester
+    auth.login('tester', '123')
+
+    # Create a habit
+    client.post('/', data={'new_habit': 'Gym'})
+
+    # Confirm habit is in database
+    assert Habit.query.filter_by(name= 'Gym').count() == 1
+
+
+def test_delete_habit(client, auth):
+    # Log in dummy tester
+    auth.login('tester', '123')
+
+    # Create a habit
+    client.post('/', data={'new_habit': 'Gym'})
+    # Confirm habit is in database
+    assert Habit.query.filter_by(name= 'Gym').count() == 1
+
+    # Fetch test habit
+    habit = Habit.query.first()
+
+    # Delete test habit
+    client.post(f'/delete/{habit.id}')
+    # Confirm habit is removed from database
+    assert Habit.query.count() == 0
+
+    
+
