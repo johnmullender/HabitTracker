25fa026a2f3b38b77824360835dc400f9cdcdd68
johnmullender <johnmullender2x@gmail.com>
2026-02-01 03:49:26 -0600

    Added unit tests for habit CRUD (empty habit name, long habit name, habit name w/ special characters / html, deleting ghost habit, repeat habit completion streak logic)

--- tests/test_habits.py
+++ tests/test_habits.py
@@ -1,9 +1,11 @@
 from app import Habit, app, User, db
 from datetime import date
 
+
+### Test basic habit creation ###
 def test_add_habit(client, auth):
     # Log in dummy tester
-    auth.login('tester', '123')
+    auth.login('tester', '12345678')
 
     # Create a habit
     client.post('/', data={'new_habit': 'Gym'})
@@ -12,9 +14,10 @@ def test_add_habit(client, auth):
     assert Habit.query.filter_by(name= 'Gym').count() == 1
 
 
+### Test basic habit deletion ###
 def test_delete_habit(client, auth):
     # Log in dummy tester
-    auth.login('tester', '123')
+    auth.login('tester', '12345678')
 
     # Create a habit
     client.post('/', data={'new_habit': 'Gym'})
@@ -30,11 +33,12 @@ def test_delete_habit(client, auth):
     assert Habit.query.count() == 0
 
 
+### Test malicious habit deletion ###
 def test_delete_other_users_habit(client):
     with app.app_context():
         # Create 2 Users and add them to database
-        attacker = User(username= 'attacker', password= '123')
-        victim = User(username= 'victim', password= '123')
+        attacker = User(username= 'attacker', password= '12345678')
+        victim = User(username= 'victim', password= '12345678')
         db.session.add(attacker)
         db.session.add(victim)
         db.session.commit()
@@ -56,3 +60,77 @@ def test_delete_other_users_habit(client):
         assert Habit.query.filter_by(id= victim_habit_id).first() is not None
     
 
+### Test empty habit creation ###
+def test_habit_empty_name(client, auth):
+    # Log in dummy tester
+    auth.login('tester', '12345678')
+
+    # Attempt to create empty habit
+    response = client.post('/', data={'new_habit': ' '}, follow_redirects=True)
+    assert b'Habit cannot be empty' in response.data
+    assert Habit.query.count() == 0
+
+
+### Test habit name over 30 characters ###
+def test_habit_long_name(client, auth):
+    # Log in dummy tester
+    auth.login('tester', '12345678')
+
+    # Attempt to create habit with name > 30 characters
+    response = client.post('/', data={'new_habit': "a" * 120}, follow_redirects= True)
+    assert b'Habit cannot be more than 30 characters' in response.data
+    assert Habit.query.count() == 0
+
+
+### Test habit with special characters ###
+def test_habit_special_char(client, auth):
+    # Log in dummy tester
+    auth.login('tester', '12345678')
+
+    # Attempt to create habit with special characters
+    client.post('/', data={'new_habit': 'Gym @ 7'})
+    assert Habit.query.filter_by(name= 'Gym @ 7').count() == 1
+
+    # Attempt to create habit with html input
+    response = client.post('/', data={'new_habit': '<script>alert(1)</script>'}, follow_redirects= True)
+    assert Habit.query.filter_by(name= '<script>alert(1)</script>').count() == 1
+    # Verify safe version is still visible
+    assert '&lt;script&gt;alert(1)&lt;/script&gt;' in response.get_data(as_text=True)
+
+
+### Test attempt to delete a non-existent habit ###
+def test_delete_ghost_habit(auth, client):
+    # Log in dummy tester
+    auth.login('tester', '12345678')
+
+    # Attempt to delete habit that does not exist in db
+    response = client.post('/delete/99', follow_redirects= True)
+    assert b'Welcome' in response.data
+
+
+### Test repeat habit marked done ###
+def test_repeat_habit_completion(auth, client):
+    auth.login('tester', '12345678')
+
+    with app.app_context():
+        tester = User.query.filter_by(username= 'tester').first()
+        tester_id = tester.id
+        # Add test habit to database
+        test_habit = Habit(name= 'gym', user_id= tester_id, date_created= date.today())
+        db.session.add(test_habit)
+        db.session.commit()
+
+        habit_id = test_habit.id
+
+    # Tester logs habit
+    client.post(f'/done/{habit_id}')
+    with app.app_context():
+        habit =  Habit.query.filter_by(name= 'gym').first()
+        assert habit.streak == 1
+
+
+    # Tester logs habit again (same day)
+    client.post(f'/done/{habit_id}')
+    with app.app_context():
+        habit =  Habit.query.filter_by(name= 'gym').first()
+        assert habit.streak == 1
